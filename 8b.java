7400e424ecce0a4a70eaa1971950b5b08408c33a
aditya <125145750+aditya07389@users.noreply.github.com>
2024-07-19 09:17:13 +0530

    Create 8b.java

---
+++ 8b.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public int subarraySum(int[] nums, int k) {
+        Map<Integer, Integer> sumCount = new HashMap<>();
+        int sum = 0;
+        int count = 0;
+        
+   
+        sumCount.put(0, 1);
+        
+        for (int num : nums) {
+            sum += num;
+            if (sumCount.containsKey(sum - k)) {
+                count += sumCount.get(sum - k);
+            }
+            sumCount.put(sum, sumCount.getOrDefault(sum, 0) + 1);
+        }
+        
+        return count;
+    }
+}
